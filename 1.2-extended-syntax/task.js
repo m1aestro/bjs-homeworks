508ca6c35dc5cbb6c4aecf6afb9ed506981b8749
Alexandr Avakyan <avakjan@mail.ru>
2020-08-09 16:12:03 +0300

    Homework 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,50 @@
+"use strict"
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    // Задача №1
+    let x = [];
+    let D = Math.pow(b, 2) - 4 * a * c;
+
+    if (D === 0) {
+        x.push(-b / (2 * a));
+    } else if (D > 0) {
+        x.push((-b + Math.sqrt(D)) / 2 * a);
+        x.push((-b - Math.sqrt(D)) / 2 * a);
+    }
+
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    // Задача №2
+    let sum = 0;
+    let averageMark = 0;
+
+    if (marks.length === 0) {
+        return 0;
+    } else if (marks.length > 5) {
+        console.log('Больше 5 оценок')
+        marks.splice(5);
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i]
+    }
+
+    averageMark = sum / marks.length
+
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    // Задача №3
+    let result = '';
+
+    if (new Date().getFullYear() - dateOfBirthday.getFullYear() > 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+
+    return result;
 }
