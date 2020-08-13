b4e37e766bce2a6f52b689e51fbc18e6f48a9281
Alexandr Avakyan <avakjan@mail.ru>
2020-08-13 17:02:43 +0300

    Homework 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,26 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    // Задача №1
+    let check = {'Процентная ставка': percent, 'Начальный взнос': contribution, 'Общая стоимость': amount};
+    for (let key in check) {
+        if (isNaN(check[key]))
+            return `Параметр ${key} содержит неправильное значение ${check[key]}`;
+    }
+
+    let month = (date.getFullYear() - new Date().getFullYear()) * 12 + date.getMonth() - new Date().getMonth();
+    let percentPerMonth = percent / 100 / 12;
+    let payment = (amount - contribution) * (percentPerMonth + percentPerMonth /(Math.pow(1 + percentPerMonth, month) - 1));
+    let totalAmount = parseFloat((payment * month).toFixed(2));
+    
+    console.log(totalAmount);
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    // Задача №2
+    
+    return `Привет, мир! Меня зовут ${name ? name : 'Аноним'}`;
+
 }
